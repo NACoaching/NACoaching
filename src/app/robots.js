c217cf454e7fb6859b02eb449a367da1fa5bc495
Nolwen Albanesi <nolwenalbanesi@macbookair.home>
2026-05-21 17:35:25 +0200

    fix(seo): exclude /success checkout confirmation page from search indexing

--- src/app/robots.js
+++ src/app/robots.js
@@ -4,7 +4,7 @@ export default function robots() {
             {
                 userAgent: '*',
                 allow: '/',
-                disallow: ['/admin/', '/api/'],
+                disallow: ['/admin/', '/api/', '/success'],
             }
         ],
         sitemap: 'https://www.na-coaching.com/sitemap.xml',
